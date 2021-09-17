68a512bedd6e00fb4e07983579d39ea893819a92
kodijeee <khodijah.aulia.rahma-2020@vokasi.unair.ac.id>
2021-09-17 19:25:39 +0700

    commit2

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,7 +13,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        dd('Hallo semuanya, ini Controller Kelompok 6');
+        dd('Hallo , ini Controller Kelompok 6');
     }
 
     /**
