09777f121a9279ce1f9ee47a9fcbef95ddd039bc
kodijeee <khodijah.aulia.rahma-2020@vokasi.unair.ac.id>
2021-09-17 15:46:04 +0700

    commitcontroller

--- app/Http/Controllers/testing.php
+++ app/Http/Controllers/testing.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class HomeController extends Controller
+class testing extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -13,7 +13,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        dd('Halo, ini Controller Kelompok 6');
+        dd('Halo ini controller testing');
     }
 
     /**
